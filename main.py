2644b9707c2cdc155b0d5056a05a147cf95eb0c5
anderskate <andrey.starozhilov@mail.ru>
2021-05-24 08:37:17 +0500

    feat: add abillity to create intents for dialog flow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler, Filters
+from google.cloud import dialogflow
 import logging
 
 
@@ -24,10 +25,56 @@ class DialogBot:
 
     def echo(self, bot, update):
         """"""
+        project_id = 'tactical-codex-313718'
+        session_id = 590772216
+        user_text = update.message.text
+        language_code = 'ru'
+        bot_response = self.detect_intent_texts(
+            project_id, session_id,
+            user_text, language_code
+        )
+
         bot.send_message(
             chat_id=update.message.chat_id,
-            text=update.message.text
+            text=bot_response,
         )
+
+    def detect_intent_texts(self, project_id, session_id, text, language_code):
+        """Returns the result of detect intent with texts as inputs.
+
+        Using the same `session_id` between requests allows continuation
+        of the conversation."""
+        session_client = dialogflow.SessionsClient()
+
+        session = session_client.session_path(project_id, session_id)
+        print("Session path: {}\n".format(session))
+
+
+        text_input = dialogflow.TextInput(
+            text=text,
+            language_code=language_code,
+        )
+
+        query_input = dialogflow.QueryInput(
+            text=text_input
+        )
+
+        response = session_client.detect_intent(
+            request={"session": session, "query_input": query_input}
+        )
+
+        print("=" * 20)
+        print("Query text: {}".format(response.query_result.query_text))
+        print(
+            "Detected intent: {} (confidence: {})\n".format(
+                response.query_result.intent.display_name,
+                response.query_result.intent_detection_confidence,
+            )
+        )
+        print("Fulfillment text: {}\n".format(
+            response.query_result.fulfillment_text))
+
+        return response.query_result.fulfillment_text
 
     def start_chat(self):
         self.updater.start_polling()
@@ -40,7 +87,7 @@ def main():
         level=logging.INFO
     )
 
-    bot_token = '1720558878:AAH-sE9uPV2rhs2VzURnX_0N6krP9NMxzuw'
+    bot_token = ''
     dialog_bot = DialogBot(bot_token)
     dialog_bot.start_chat()
 
---
+++ utils.py
@@ -0,0 +1,58 @@
+import json
+from google.cloud import dialogflow
+
+
+def get_data_from_file(file_path):
+    """"""
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    return data
+
+
+def create_intent(
+        project_id, display_name,
+        training_phrases_parts, message_texts):
+    """Create an intent of the given intent type."""
+
+    intents_client = dialogflow.IntentsClient()
+
+    parent = dialogflow.AgentsClient.agent_path(project_id)
+    training_phrases = []
+    for training_phrases_part in training_phrases_parts:
+        part = dialogflow.Intent.TrainingPhrase.Part(
+            text=training_phrases_part
+        )
+        # Here we create a new training phrase for each provided part.
+        training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
+        training_phrases.append(training_phrase)
+
+    text = dialogflow.Intent.Message.Text(text=[message_texts])
+
+    message = dialogflow.Intent.Message(text=text)
+
+    intent = dialogflow.Intent(
+        display_name=display_name,
+        training_phrases=training_phrases,
+        messages=[message],
+    )
+
+    response = intents_client.create_intent(
+        request={"parent": parent, "intent": intent}
+    )
+
+    print("Intent created: {}".format(response))
+
+
+def main():
+    data = get_data_from_file('chat_questions.json')
+    project_id = ''
+    display_name = 'Устройство на работу'
+    create_intent(
+        project_id, display_name,
+        data[display_name]['questions'],
+        data[display_name]['answer'],
+    )
+
+
+if __name__ == '__main__':
+    main()
